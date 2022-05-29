5de071f712501eeaa361e784bf1850d1966ae601
Jennie Mauch <jenniemauch@me.com>
2022-05-29 15:37:59 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// create connection to our db
+const Sequelize = require('sequelize');
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3001
+      dialectOptions: {
+        decimalNumbers: true,
+      },
     });
 
 module.exports = sequelize;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -21,12 +21,12 @@ router.get('/', (req, res) => {
           attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
           include: {
             model: User,
-            attributes: ['username', 'twitter', 'github']
+            attributes: ['username', 'email']
           }
         },
         {
           model: User,
-          attributes: ['username', 'twitter', 'github']
+          attributes: ['username', 'email']
         },
       ]
     })
@@ -52,14 +52,14 @@ router.get('/', (req, res) => {
         // include the Comment model here:
         {
           model: User,
-          attributes: ['username', 'twitter', 'github']
+          attributes: ['username', 'email']
         },
         {
           model: Comment,
           attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
           include: {
             model: User,
-            attributes: ['username', 'twitter', 'github']
+            attributes: ['username', 'email', ]
           }
         }
       ]
